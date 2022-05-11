ef785c32abd2daaf946d108d9223ad66feafdc21
Javier Morales <javi_morales@hotmail.es>
2022-05-11 21:22:34 +0200

    Six hours delay added

--- Firmware_STM32CubeIDE/F303_LED_Driver/Core/Src/main.c
+++ Firmware_STM32CubeIDE/F303_LED_Driver/Core/Src/main.c
@@ -120,7 +120,7 @@ int main(void)
 	HAL_TIM_PWM_Start(&htim8, TIM_CHANNEL_1);
 	HAL_TIMEx_PWMN_Start(&htim8, TIM_CHANNEL_1);
 
-	HAL_Delay(60*60*1000);
+	HAL_Delay(6*60*60*1000);
 	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
 
 //	/* Stop both PWM signals */
